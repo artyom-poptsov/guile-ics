6bb6b3b4b40ce8edd9039c5b97a830ba7263fc5d
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2016-12-04 07:40:54 +0300

    ical-object.scm: Add docstrings
    * modules/ics/ical-object.scm (->ical-object, ical-object-icalprops)
      (ical-object-component): Add docstrings.

--- modules/ics/ical-object.scm
+++ modules/ics/ical-object.scm
@@ -25,14 +25,17 @@
             ->ical-object))
 
 (define (->ical-object icalprops component)
+  "Convert an ICALPROPS list and a COMPONENT alist to an iCal object."
   (list (cons 'ICALPROPS (list icalprops))
         (cons 'COMPONENT (list component))))
 
 
 (define (ical-object-icalprops ical-object)
+  "Get iCal properties from an ICAL-OBJECT."
   (cadr (assoc 'ICALPROPS ical-object)))
 
 (define (ical-object-component ical-object)
+  "Get an iCal component alist from ICAL-OBJECT."
   (cadr (assoc 'COMPONENT ical-object)))
 
 ;;; ical-object.scm ends here.
