fb9b2bc86e0b3bf634e0e97af9eb9d8282eee3fd
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2017-01-06 03:24:25 +0300

    conv.scm: New module
    * modules/ics/conv.scm: New module.
    * modules/ics/Makefile.am (SCM_SOURCES): Add 'conv.scm'.
    * utils/ics.in: Use the new module.  Handle 'to-org-mode' option.

---
+++ modules/ics/conv.scm
@@ -0,0 +1,63 @@
+;;; conv.scm -- Various converters for iCalendar objects.
+
+;; Copyright (C) 2017 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Various converters for iCalendar objects.
+
+
+;;; Code:
+
+
+(define-module (ics conv)
+  #:use-module (ice-9 rdelim)
+  #:use-module (ics ical-object)
+  #:export (ical-object->org-mode))
+
+(define* (ical-object->org-mode ical-object
+                                #:optional (port (current-output-port)))
+  "Convert an ICAL-OBJECT object to org-mode format and print the
+results to a PORT."
+  (define (print-icalprops props current-indent)
+    (let ((i (make-string current-indent #\space)))
+      (format port "~a:PROPERTIES:~%" i)
+      (for-each (lambda (e)
+                  (format port "~a:~a: ~a~%" i (car e) (cdr e)))
+                props)
+      (format port "~a:END:~%" i)))
+  (define (print-components components current-indent level)
+    (let ((i (make-string current-indent #\space))
+          (s (make-string current-indent #\*)))
+      (for-each (lambda (component)
+                  (let ((cname  (car component))
+                        (object (cdr component)))
+                    (format port "~a ~a~%" s cname)
+                    (print-icalprops (ical-object-icalprops object)
+                                     (+ current-indent 1))
+                    (print-components (ical-object-component object)
+                                      (+ current-indent 1)
+                                      (1+ level))))
+                components)))
+  (define (print-vcalendar)
+    (write-line "* VCALENDAR" port)
+    (print-icalprops (ical-object-icalprops ical-object) 2)
+    (print-components (ical-object-component ical-object) 2 1))
+
+  (print-vcalendar))
+
+;;; conv.scm ends here.
