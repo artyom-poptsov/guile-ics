e7cd925ab5a9c2dfb55766e04b99703d86325884
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2017-02-18 07:42:50 +0300

    modules/ics/ical-object.scm: Update copyright dates

--- modules/ics/ical-object.scm
+++ modules/ics/ical-object.scm
@@ -1,4 +1,4 @@
-;; Copyright (C) 2016 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;; Copyright (C) 2016, 2017 Artyom V. Poptsov <poptsov.artyom@gmail.com>
 ;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
