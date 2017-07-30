089732428fa3ebf1c4231e5627866e0be61f09b1
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2017-07-31 00:44:44 +0300

    ical-property.scm (ical-property->string): Add a docstring
    * modules/ics/ical-property.scm (ical-property->string): Add a
      docstring.

--- modules/ics/ical-property.scm
+++ modules/ics/ical-property.scm
@@ -29,16 +29,17 @@
 parameter value, or return #f if no parameter found."
   (assoc-ref name (ical-property-parameters ical-property)))
 
-(define-method (ical-property->string (obj <ical-property>))
+(define-method (ical-property->string (ical-property <ical-property>))
+  "Convert an ICAL-PROPERTY to a iCalendar string, return the string."
   (define (parameters->string parameters)
     (string-join (map (lambda (parameter)
                         (format #f "~a=~a" (car parameter)
                                 (cdr parameter)))
                       parameters)
                  ";"))
-  (let ((parameters (ical-property-parameters obj))
-        (name       (ical-property-name obj))
-        (value      (ical-property-value obj)))
+  (let ((parameters (ical-property-parameters ical-property))
+        (name       (ical-property-name ical-property))
+        (value      (ical-property-value ical-property)))
     (if parameters
         (format #f "~a;~a:~a" name (parameters->string parameters) value)
         (string-append name ":" value))))
