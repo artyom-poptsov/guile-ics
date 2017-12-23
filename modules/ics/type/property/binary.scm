e25a8c5d0f9567e6ae22ecf040b4f1731dc0d46d
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2017-12-23 05:19:59 +0300

    binary.scm (ics-property->ics-property:binary): Fix an error
    * modules/ics/type/property/binary.scm
      (ics-property->ics-property:binary): Remove extra paren.

--- modules/ics/type/property/binary.scm
+++ modules/ics/type/property/binary.scm
@@ -90,8 +90,8 @@ properties are identical, #f otherwise."
     #:name        (ics-property-name          property)
     #:parameters  (ics-property-parameters    property)
     #:format-type (ics-property-parameter-ref property 'FMTTYPE)
-    #:encoding    (ics-property-parameter-ref property 'ENCODING))
-    #:value       (ics-property-value         property))
+    #:encoding    (ics-property-parameter-ref property 'ENCODING)
+    #:value       (ics-property-value         property)))
 
 (define-method (ics-property:binary->ics-property
                 (property <ics-property:binary>))
