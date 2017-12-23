fa663fecf03c44b68706769ce12cf85b33ca01f5
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2017-12-23 05:46:58 +0300

    property.scm (ics-property->typed-property): Improve
    * modules/ics/type/property.scm (ics-property->typed-property): Don't
      do anything if property is already converted.
      (ics-typed-property->ics-property): Likewise.

--- modules/ics/type/property.scm
+++ modules/ics/type/property.scm
@@ -155,22 +155,26 @@
     (FLOAT       . ,ics-property:float->ics-property)))
 
 (define-method (ics-property->typed-property (property <ics-property>))
-  (let* ((type (ics-property-type property))
-         (converter (assoc-ref %converters-to-typed type)))
-    (unless converter
-      (error 'guile-ics-error
-             "Converter for the property type is not defined yet"
-             type))
-    (converter property)))
+  (let ((type (ics-property-type property)))
+    (if type
+        property
+        (let ((converter (assoc-ref %converters-to-typed type)))
+          (unless converter
+            (error 'guile-ics-error
+                   "Converter for the property type is not defined yet"
+                   type))
+          (converter property)))))
 
 (define-method (ics-typed-property->ics-property (property <ics-property>))
-  (let* ((type      (ics-property-type property))
-         (converter (assoc-ref %converters-to-untyped type)))
-    (unless converter
-      (error 'guile-ics-error
-             "Converter for the property type is not defined yet"
-             type))
-    (converter property)))
+  (let ((type (ics-property-type property)))
+    (if type
+        (let ((converter (assoc-ref %converters-to-untyped type)))
+          (unless converter
+            (error 'guile-ics-error
+                   "Converter for the property type is not defined yet"
+                   type))
+          (converter property))
+        property)))
 
 
 ;;;
