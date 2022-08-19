55a455ed8743da2162d9579a68e207b38ff16e8f
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-08-19 22:12:28 +0300

    modules/ics/fsm: Add a basic ICS stream parser
    * modules/ics/fsm/Makefile.am: Update.
    * modules/ics/fsm/stream-context.scm: Add.
    * modules/ics/fsm/stream-parser.puml: Add.
    * tests/stream-context.scm: New test suite.
    * tests/Makefile.am: Update.

---
+++ modules/ics/fsm/stream-context.scm
@@ -0,0 +1,149 @@
+(define-module (ics fsm stream-context)
+  #:use-module (srfi srfi-26)
+  #:use-module (oop goops)
+  #:use-module (ics common)
+  #:use-module (ics type object)
+  #:use-module (ics type property)
+  #:use-module (ics parser)
+  #:use-module (ics fsm context)
+  #:use-module (ics fsm content-line-parser-context)
+  #:use-module (ics fsm content-line-parser)
+  #:use-module (ics type object)
+  #:export (<stream-context>
+            stream-context?
+            stream-context-append-object!
+            stream-context-port
+            stream-context-current-object
+            stream-context-current-object-set!
+            stream-context-objects
+            stream-context-objects-set!
+            stream-context-objects-count
+
+            ;; FSM event sources.
+            stream:read
+            stream:dummy-event-source
+
+            ;; Predicatres and FSM guards.
+            stream-parser-context?
+            stream:eof-object?
+            stream:vcalendar-begin?
+            stream:vcalendar-end?
+            stream:vevent-begin?
+            stream:vevent-end?
+
+            ;; FSM actions.
+            stream:create-object
+            stream:append-object
+            stream:append-property
+
+            ;; FSM error handling.
+            stream:error
+            stream:error-unexpected-eof-in-object))
+
+
+(define-class <stream-context> (<context>)
+  ;; <port>
+  (port
+   #:init-thunk   (lambda () (current-input-port))
+   #:init-keyword #:port
+   #:getter       stream-context-port)
+
+  ;; <ics-object> | #f
+  (current-object
+   #:init-value   #f
+   #:init-keyword #:current-object
+   #:getter       stream-context-current-object
+   #:setter       stream-context-current-object-set!)
+
+  ;; <list>
+  (objects
+   #:init-value   '()
+   #:init-keyword #:objects
+   #:getter       stream-context-objects
+   #:setter       stream-context-objects-set!))
+
+(define (stream-context? x)
+  (is-a? x <stream-context>))
+
+(define-method (stream-context-append-object! (context <stream-context>))
+  (let ((current-object (stream-context-current-object context))
+        (objects        (stream-context-objects context)))
+    (stream-context-objects-set! context (append objects (list current-object)))
+    context))
+
+(define-method (stream-context-objects-count (context <stream-context>))
+  (length (stream-context-objects context)))
+
+
+(define (stream:read ctx)
+  "Event source for the ICS stream parser."
+  (fsm-run! (make <content-line-parser>)
+            (make <content-line-context>
+              #:port (stream-context-port ctx))))
+
+(define (stream:dummy-event-source ctx)
+  #t)
+
+
+;;; Guards.
+
+(define (stream:eof-object? ctx content-line-ctx)
+  (content-line-context-eof? content-line-parser-ctx))
+
+(define (stream:vcalendar-begin? ctx content-line-ctx)
+  (content-line-vcalendar-begin?
+   (content-line-parser-context-result content-line-ctx)))
+
+(define (stream:vcalendar-end? ctx content-line-ctx)
+  (content-line-vcalendar-end?
+   (content-line-parser-context-result content-line-ctx)))
+
+(define (stream:vevent-begin? ctx content-line-ctx)
+  (content-line-vevent-begin?
+   (content-line-parser-context-result content-line-ctx)))
+
+(define (stream:vevent-end? ctx content-line-ctx)
+  (content-line-vevent-end?
+   (content-line-parser-context-result content-line-ctx)))
+
+
+;;; Actions
+
+(define (stream:create-object ctx content-line-ctx)
+  (let* ((content-line (content-line-context-result content-line-ctx))
+         (name         (content-line-name content-line)))
+    (stream-context-current-object-set! ctx (make <ics-object>
+                                              #:name name)))
+  ctx)
+
+(define (stream:append-object ctx content-line-ctx)
+  (stream-context-append-object! ctx)
+  (stream-context-current-object-set! ctx #f)
+  ctx)
+
+(define (stream:append-property ctx content-line-ctx)
+  (let* ((content-line (content-line-context-result content-line-ctx))
+         (property (make <ics-property>
+                     #:name       (content-line-name content-line)
+                     #:value      (content-line-value content-line)
+                     #:parameters (content-line-parameters content-line)))
+         (current-object (stream-context-current-object ctx))
+         (current-object-properties (ics-object-properties current-object)))
+    (ics-object-properties-set! current-object
+                                (cons property current-object-properties))
+    ctx))
+
+
+;;; Error handlers.
+
+(define (stream:error ctx content-line-parser-ctx)
+  (error "Unexpected content in a stream"
+         ctx
+         content-line-parser-ctx))
+
+(define (stream:error-unexpected-eof-in-object ctx content-line-parser-ctx)
+  (error "Unexpected EOF in ICalendar object"
+         ctx
+         content-line-parser-ctx))
+
+;;; stream-context.scm ends here.
---
+++ tests/stream-context.scm
@@ -0,0 +1,108 @@
+;;; ics.scm -- Tests for ICS parser.
+
+;; Copyright (C) 2022 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+
+;;; Code:
+
+(use-modules (srfi srfi-64)
+             (oop goops)
+             (ics type object)
+             (ics fsm context)
+             (ics fsm content-line-parser-context)
+             (ics fsm stream-context))
+
+
+(define %test-suite-name "stream-context")
+
+(test-begin %test-suite-name)
+
+
+
+(test-assert "stream-context?"
+  (stream-context? (make <stream-context>)))
+
+(test-assert "stream:create-object"
+  (let* ((content-line     (make <content-line>
+                             #:name "BEGIN"
+                             #:value "VCALENDAR"))
+         (content-line-ctx (make <content-line-context>
+                             #:content-line content-line))
+         (ctx              (stream:create-object (make <stream-context>)
+                                                 content-line-ctx)))
+    (ics-object? (stream-context-current-object ctx))))
+
+(test-equal "stream-context-objects-count"
+  1
+  (let ((ctx (make <stream-context>
+               #:objects (list (make <ics-object>
+                                 #:name "BEGIN"
+                                 #:value "VCALENDAR")))))
+    (stream-context-objects-count ctx)))
+
+(test-assert "stream:append-object"
+  (let* ((content-line     (make <content-line>
+                             #:name "BEGIN"
+                             #:value "VCALENDAR"))
+         (content-line-ctx (make <content-line-context>
+                             #:content-line content-line))
+         (ctx              (stream:create-object (make <stream-context>)
+                                                 content-line-ctx)))
+    (stream:append-object ctx #f)
+    (and (not (stream-context-current-object ctx))
+         (= (stream-context-objects-count ctx) 1)
+         (ics-object? (car (stream-context-objects ctx))))))
+
+(test-assert "stream:append-property"
+  (let ((ctx (make <stream-context>
+               #:current-object (make <ics-object>
+                                  #:name  "BEGIN"
+                                  #:value "VCALENDAR")))
+        (content-line-ctx (make <content-line-context>
+                            #:content-line (make <content-line>
+                                             #:name "VERSION"
+                                             #:value "2.0"))))
+    (stream:append-property ctx content-line-ctx)
+    (let ((current-object (stream-context-current-object ctx)))
+      (and (ics-object? current-object)
+           (= (length (ics-object-properties current-object)) 1)))))
+
+
+(test-assert "stream:read"
+  (with-input-from-string
+      (string-append
+       "BEGIN:VCALENDAR\r\n"
+       "VERSION:2.0\r\n"
+       "END:VCALENDAR\r\n")
+    (lambda ()
+      (let ((ctx (stream:read (make <stream-context>
+                                #:port (current-input-port)))))
+        (and (content-line-context? ctx)
+             (content-line? (content-line-context-result ctx)))))))
+
+
+(define exit-status (test-runner-fail-count (test-runner-current)))
+
+(test-end %test-suite-name)
+
+(exit exit-status)
+
+;;; stream-context.scm ends here.
+
+
